173e816ea768c189ad31a92d3ad86d77a64e0ba6
slametrianto <slametrianto25@gmail.com>
2020-02-01 10:42:23 +0700

    engine blade laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layout.master')
+
+
+@section('title', 'Home')
+
+
+
+@section('content')
+
+<div class="container">
+<h1>Home</h1>
+</div>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi deleniti numquam porro dolorem expedita consequuntur error laboriosam aspernatur et, cumque voluptates provident sunt similique adipisci, atque aperiam quis voluptas reprehenderit.</p>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas suscipit ut earum rerum ea excepturi, eum eos iste obcaecati eaque beatae rem ipsum nesciunt at nam. Dolor culpa iusto distinctio.</p>
+
+
+@endsection
